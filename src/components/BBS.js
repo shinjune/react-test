21e68fb5503b88e99cfe50cb9761a88a4bf6c66a
Kim Seungha <seungha.me@gmail.com>
2018-01-18 14:39:23 +0900

    firebase 설치

--- src/components/BBS.js
+++ src/components/BBS.js
@@ -1,6 +1,18 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
 
 export default class BBS extends Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyC5bvpoKyfa3qwTxhSt0PxgQZI2dI3QbZc",
+      authDomain: "fds-cra.firebaseapp.com",
+      databaseURL: "https://fds-cra.firebaseio.com",
+      projectId: "fds-cra",
+      storageBucket: "fds-cra.appspot.com",
+      messagingSenderId: "966283711333"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <div>BBS</div>
